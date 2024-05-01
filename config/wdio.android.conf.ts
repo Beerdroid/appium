7baa3969ef7c8fd03366abfc3e3103f821bb1230
dzmit <dzmitry.auchynnikau@gmail.com>
2024-05-01 09:18:06 +0200

    first commit

---
+++ config/wdio.android.conf.ts
@@ -0,0 +1,32 @@
+import { join } from 'path';
+import {config} from './wdio.appium.local.conf.ts';
+
+// ============
+// Specs
+// ============
+config.specs = [
+    '../test/specs/*.ts',
+];
+
+// ============
+// Capabilities
+// ============
+config.capabilities = [
+    {
+        // The defaults you need to have in your config
+        platformName: 'Android',
+        'appium:avd': 'Pixel_3a_API_34_extension_level_7_x86_64',
+        'appium:platformVersion': '14.0',
+        'appium:appPackage': 'com.android.settings',
+        'appium:orientation': 'PORTRAIT',
+        'appium:automationName': 'UiAutomator2',
+        // The path to the app
+        'appium:app': join(process.cwd(), './apps/android.wdio.native.app.v1.0.8.apk'),
+        // @ts-ignore
+        'appium:appWaitActivity': 'com.wdiodemoapp.MainActivity',
+        'appium:noReset': true,
+        'appium:newCommandTimeout': 240,
+    },
+];
+
+exports.config= config;
---
+++ test/specs/android.spec.ts
@@ -0,0 +1,23 @@
+import HomePage from "../../src/pages/home.page";
+import LoginPage from "../../src/pages/login.page";
+import SwipePage from "../../src/pages/swipe.page";
+
+describe("Android Mobile automation tests", () => {
+    it("should check the title on home page", async () => {
+        expect(await HomePage.getTitle()).toHaveText("WEBDRIVER");
+    });
+
+    it("should login into the app successfully", async () => {
+        await HomePage.openLoginScreen();
+        await LoginPage.login("Test@email.com", "Pass12345");
+        expect(await LoginPage.successMessageTitle()).toEqual("Success");
+        expect(await LoginPage.successMessage()).toEqual("You are logged in!");
+        await LoginPage.closePopup();
+    });
+
+    it("should be able to swipe based on element displayed", async () => {
+        await HomePage.openSwipeMenu();
+        await SwipePage.assertCommunityTextIsDisplayed();
+        expect(await SwipePage.greatCommunityText()).toEqual("GREAT COMMUNITY");
+    });
+});
